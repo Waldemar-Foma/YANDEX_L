b656780270b474e8ac163023d1103bb6b2e58364
Foma <104798936+Waldemar-Foma@users.noreply.github.com>
2025-01-07 22:41:05 +0200

    Create Равенство и порядок.py

---
+++ Повторение. Проектирование классов/Равенство и порядок.py
@@ -0,0 +1,33 @@
+class Point:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"{self.name}({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"Point('{self.name}', {self.x}, {self.y})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return (self.name, self.x, self.y) == (other.name, other.x, other.y)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return (self.name, self.x, self.y) < (other.name, other.x, other.y)
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __gt__(self, other):
+        return not self <= other
+
+    def __ge__(self, other):
+        return not self < other
